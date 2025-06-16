14f9155bbf2428076168f8c7406c12d93a84a70e
Abdul Rofiq <abdul.rofiq2308@gmail.com>
2025-06-16 13:14:07 +0700

    feat: angular 14

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  standalone: true,
+  imports: [CommonModule],
+  template: '<h1>Hello Angular 14 (Standalone)!</h1>'
 })
 export class AppComponent {
   title = 'proyek-pertamaku';
